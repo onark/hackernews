f7444b4e10c1449d4123412e9fc881a16094f67f
Onur <karalaron@gmail.com>
2023-01-10 08:09:40 +0200

    loading indicator

--- src/index.js
+++ src/index.js
@@ -3,23 +3,27 @@ import { createRoot } from 'react-dom/client'
 import storyService from './services/stories'
 import Pagination from './components/Pagination'
 import News from './components/News'
+import LoadingSpinner from './components/LoadingSpinner'
 import './styles.scss'
 
 const App = () => {
   const [page, setPage] = useState(1);
   const [stories, setStories] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchStories = async () => {
+      setLoading(true);
       const newStories = await storyService.getAll(page);
       setStories(newStories);
+      setLoading(false);
     };
     fetchStories();
   }, [page]);
 
   return (
     <div className="content-wrapper">
-      <News stories={stories}/>
+      {loading ? <LoadingSpinner /> : <News stories={stories}/>}
       <Pagination page={page} setPage={setPage}/>
     </div>
   );
